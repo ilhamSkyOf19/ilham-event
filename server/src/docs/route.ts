15871eea96dd53e48b3bbc4ea074bcd48390a190
ilham rohmatulloh <ilhamm778866@gmail.com>
2025-11-30 16:20:02 +0700

    add swagger-ui-dist

--- server/src/docs/route.ts
+++ server/src/docs/route.ts
@@ -1,4 +1,5 @@
-import { Express } from "express";
+import path from "path";
+import express, { Express } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger_output.json";
 import swaggerDist from "swagger-ui-dist";
@@ -6,9 +7,17 @@ import swaggerDist from "swagger-ui-dist";
 export default function docs(app: Express) {
   const swaggerDistPath = swaggerDist.absolutePath();
 
+  // serve seluruh folder swagger-ui-dist
+  app.use("/swagger-ui-dist", express.static(swaggerDistPath));
+
+  // setup Swagger UI
   app.use(
     "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument, undefined, undefined, swaggerDistPath)
+    swaggerUi.serveFiles(swaggerDocument, { swaggerUrl: "/api-docs" }),
+    swaggerUi.setup(swaggerDocument, {
+      customCssUrl: "/swagger-ui-dist/swagger-ui.css",
+      customJs: "/swagger-ui-dist/swagger-ui-bundle.js",
+      customfavIcon: "/swagger-ui-dist/favicon-32x32.png",
+    })
   );
 }
