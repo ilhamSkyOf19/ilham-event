d108a341af0153667ae5a75288c21f96abd7664c
ilham rohmatulloh <ilhamm778866@gmail.com>
2025-11-30 16:34:29 +0700

    add swagger-ui-dist

--- server/src/docs/route.ts
+++ server/src/docs/route.ts
@@ -2,12 +2,16 @@ import { Express } from "express";
 import swaggerOutput from "./swagger_output.json";
 import swaggerUi from "swagger-ui-express";
 import path from "path";
+import fs from "fs";
 
 export default function docs(app: Express) {
   // css
-  const css = path.join(
-    __dirname,
-    "../../node_modules/swagger-ui-dist/swagger-ui.css"
+  const css = fs.readFileSync(
+    path.resolve(
+      __dirname,
+      "../../node_modules/swagger-ui-dist/swagger-ui.css"
+    ),
+    "utf8"
   );
 
   app.use(
