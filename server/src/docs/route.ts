fe05665852cee1a09c797b5e1ca89d0c289adf8c
ilham rohmatulloh <ilhamm778866@gmail.com>
2025-11-30 16:48:47 +0700

    remove swagger-ui-dist

--- server/src/docs/route.ts
+++ server/src/docs/route.ts
@@ -1,24 +1,7 @@
 import { Express } from "express";
-import express from "express"; // <-- ini penting
-import swaggerUi from "swagger-ui-express";
 import swaggerOutput from "./swagger_output.json";
-import path from "path";
+import swaggerUi from "swagger-ui-express";
 
 export default function docs(app: Express) {
-  // Serve static assets
-  app.use(
-    "/swagger-ui",
-    express.static(path.join(__dirname, "../public/swagger-ui-dist"))
-  );
-
-  // Setup Swagger UI
-  app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerOutput, {
-      customCssUrl: "/swagger-ui/swagger-ui.css",
-      customJs: "/swagger-ui/swagger-ui-bundle.js",
-      customfavIcon: "/swagger-ui/favicon-32x32.png",
-    })
-  );
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 }
