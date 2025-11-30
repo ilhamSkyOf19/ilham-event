6a9bf4ebeb21927ef43c7786a7ec1f6c731665d2
ilham rohmatulloh <ilhamm778866@gmail.com>
2025-11-30 17:15:32 +0700

    add swagger dist

--- server/src/docs/route.ts
+++ server/src/docs/route.ts
@@ -3,5 +3,14 @@ import swaggerOutput from "./swagger_output.json";
 import swaggerUi from "swagger-ui-express";
 
 export default function docs(app: Express) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerOutput, {
+      customCssUrl: "/swagger-ui/swagger-ui.css",
+      customJs: "/swagger-ui/swagger-ui-bundle.js",
+      customfavIcon: "/swagger-ui/favicon-32x32.png",
+      customSiteTitle: "API Docs",
+    })
+  );
 }
