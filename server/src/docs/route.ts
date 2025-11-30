5935686bf56654a7df87c1bf7f7c036d8f29585e
ilham rohmatulloh <ilhamm778866@gmail.com>
2025-11-30 16:07:29 +0700

    membuat api untuk activate user

--- server/src/docs/route.ts
+++ server/src/docs/route.ts
@@ -1,11 +1,7 @@
-import path from "path";
+import { Express } from "express";
+import swaggerOutput from "./swagger_output.json";
 import swaggerUi from "swagger-ui-express";
-import express from "express";
-import fs from "fs";
 
-export default function docs(app: express.Express) {
-  const swaggerFilePath = path.resolve(__dirname, "swagger_output.json");
-  const swaggerOutput = JSON.parse(fs.readFileSync(swaggerFilePath, "utf-8"));
-
+export default function docs(app: Express) {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 }
