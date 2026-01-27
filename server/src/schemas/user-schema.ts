b5b7698e5573375c83f0528b47e78d90cb934a2f
ilham rohmatulloh <ilhamm778866@gmail.com>
2026-01-28 00:58:16 +0700

    update function send email

--- server/src/schemas/user-schema.ts
+++ server/src/schemas/user-schema.ts
@@ -44,7 +44,7 @@ const UserSchema = new Schema<IUser>(
   },
   {
     timestamps: true,
-  }
+  },
 );
 
 // hash password
@@ -66,7 +66,7 @@ UserSchema.post("save", async function (doc, next) {
     fullname: user.fullName,
     email: user.email,
     registeredAt: user.createdAt,
-    activationLink: `${process.env.CLIENT_URL}/auth/activation?code=${user.activeCode}`,
+    activationLink: `${process.env.BASE_URL}/auth/activation?code=${user.activeCode}`,
   });
 
   console.log("📄 TEMPLATE EMAIL LENGTH:", contentEmail.length);
