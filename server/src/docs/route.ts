ce211bb942481a32df085739509e66a03facfc01
ilham rohmatulloh <ilhamm778866@gmail.com>
2026-01-27 23:37:28 +0700

    fix bugs swagger

--- server/src/docs/route.ts
+++ server/src/docs/route.ts
@@ -3,5 +3,11 @@ import swaggerUi from "swagger-ui-express";
 import swaggerOutput from "./swagger_output.json";
 
 export default function docs(app: Express) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerOutput, {
+      explorer: true,
+    }),
+  );
 }
